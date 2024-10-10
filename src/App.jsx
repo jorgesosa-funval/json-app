a537667eca6c75d9562d54ef2f86bea7f662ed4e
Jorge Sosa <jorgesosa@fundaciondevalores.org>
2024-10-10 10:48:41 -0400

    integrate react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
 
 export default function App() {
+
   return (
-    <div className='bg-slate-700'>App</div>
+    <>
+      <Routes>
+        <Route path='/' element={<h1>Home</h1>} />
+        <Route path='/todos' element={<h1>Todos</h1>} />
+        <Route path='/posts' element={<h1>Posts</h1>} />
+        <Route path='/albums' element={<h1>Albums</h1>} />
+      </Routes>
+    </>
   )
 }
+
+
